00dd40964d2a46f79de9099758031be0f3c704c2
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-23 03:27:32 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -24,6 +24,7 @@ def bonus
 
   #code your solution here:
 [:montague][:hero][:status] = "dead"
+
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
